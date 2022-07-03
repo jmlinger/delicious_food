efe8cc86c13028ca09b5271072be1322f601c000
jmlinger <jmlinger10@gmail.com>
2022-07-03 15:41:33 -0300

    feat: implementa encriptação de password na seed de users.

--- src/database/seeders/20220701183939-users.js
+++ src/database/seeders/20220701183939-users.js
@@ -1,19 +1,21 @@
 'use strict';
 
+const { encryptPassword } = require("../../utils/bcryptServices");
+
 module.exports = {
   up: async (queryInterface, Sequelize) => queryInterface.bulkInsert('users',
     [
       {
-        name: 'Leonardo',
-        email: 'leo@test.com',
+        name: 'Frederico',
+        email: 'fred@graodireto.com.br',
         address: 'Rua das Araras, Jardim dos Pássaros, 412, Araúna, RO.',
-        password: 'leo1234',
+        password: encryptPassword('123Fred'),
       },
       {
         name: 'Eduardo',
         email: 'edu@test.com',
         address: 'Rua das Ameixas, Jardim dos Frutos, 229, Gamão, AM.',
-        password: 'edu6789',
+        password: encryptPassword('edu6789'),
       },
     ], {}),
 
