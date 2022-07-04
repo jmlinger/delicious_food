c5c9f2dc5dd93be05246c0f4241136a8d37702d1
jmlinger <jmlinger10@gmail.com>
2022-07-03 22:35:58 -0300

    style: corrige identação.

--- src/controllers/users/create.js
+++ src/controllers/users/create.js
@@ -1,9 +1,9 @@
 const userServices = require('../../services/users');
 
 module.exports = async (req, res, _next) => {
-    const user = req.body;
-  
-    const result = await userServices.create(user);
+  const user = req.body;
 
-    return res.status(result.status).json(result.message);
+  const result = await userServices.create(user);
+
+  return res.status(result.status).json(result.message);
 };
